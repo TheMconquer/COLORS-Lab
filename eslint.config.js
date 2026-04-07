93680ed81d1f85c8c5e6438d7e46b9ef1bc0a691
Tadiwanashe Mukonka <65373422+TheMconquer@users.noreply.github.com>
2026-04-07 14:25:52 -0400

    Fixed eslint config to use CommonJS syntax

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,19 @@
 module.exports = [
   {
+    languageOptions: {
+      globals: {
+        document: "readonly",
+        window: "readonly",
+        XMLHttpRequest: "readonly",
+        console: "readonly",
+        alert: "readonly"
+      }
+    },
     rules: {
       "no-unused-vars": "warn",
       "no-undef": "error",
       "semi": ["error", "always"],
-      "quotes": ["error", "double"]
+      "quotes": "off"
     }
   }
 ];
